deb836d673ee9bd90b78242f21d2ddd1eef6329f
Sébastien GELDREICH <trehinos@gmail.com>
2025-02-26 21:42:53 +0100

    Add new Euui constructors and enhance documentation
    Introduced `from_be_guids` and `from_be_bytes` constructors for Euui, allowing initialization from GUIDs or byte arrays. Updated examples and documentation for better clarity and usability. Adjusted formatting logic and dependencies to support the new functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -16,29 +16,41 @@
 //!  - or, [Euui::u64] to reach for individual u64s,
 //!  - or, [Euui::u8] or [Euui::to_be_bytes] to reach for individual u8s.
 //!
+//! ## Example
 //!
+//! ```rust
+//! use euui::Euui;
 //!
-//! ## An example
-//! ### ::format()
-//! ```txt
-//! 2f8596cc2f3b3da9adf20cf9413104ab-1f8de1116aef039d12c80587e7551080
-//! d43ed7632e94801a395a5454a382dff1-23decf62d51eafee3ec0bb98b1b90d15
+//! // Generate a zero-initialized Euui
+//! let zero_euui = Euui::default();
+//! println!("Zero Euui: {}", zero_euui);
+//!
+//! // Generate a random Euui
+//! let random_euui = Euui::random();
+//! println!("Random Euui: {}", random_euui);
+//!
+//! // Format a Euui
+//! let formatted = random_euui.format();
+//! println!("Formatted Euui:\n{}", formatted);
+//!
+//! // Access specific parts of the Euui
+//! if let Some(first_u128) = random_euui.u128(0) {
+//!     println!("First u128 of random Euui: {:032x}", first_u128);
+//! }
 //! ```
-//!
-//! ### ::to_string()
-//! ```txt
-//! 2f8596cc2f3b3da9adf20cf9413104ab1f8de1116aef039d12c80587e7551080d43ed7632e94801a395a5454a382dff123decf62d51eafee3ec0bb98b1b90d15
-//! ```
-//!
+#![no_std]
+extern crate alloc;
 
-use std::fmt::{Display, Formatter};
+use alloc::format;
+use alloc::string::String;
+use core::fmt::{Display, Formatter};
 use rand::random;
 
 /// Extended Universal Unique Identifier
 ///
 /// A 512-bits (64 bytes) identifier.
 ///
-/// A formatted EUUI follows this pattern (given #x is `self.0[x - 1]: u128`) :
+/// A formatted Euui follows this pattern (given #x is `self.0[x - 1]: u128`) :
 /// ```txt
 /// #1-#2
 /// #3-#4
@@ -49,23 +61,100 @@ use rand::random;
 pub struct Euui([u128; 4]);
 
 impl Euui {
-    /// Provides a new random 512-bits EUUI.
+    /// Generates a new random Euui.
+    ///
+    /// Each component of the Euui is generated using the `rand` crate's `random` function.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// use euui::Euui;
+    ///
+    /// let euui = Euui::random();
+    /// println!("{}", euui);
+    /// ```
     pub fn random() -> Self {
         Self([random(), random(), random(), random()])
     }
-    /// Provides a 512-bits EUUI from 4 GUIDs.
-    pub fn new(a: u128, b: u128, c: u128, d: u128) -> Self {
-        Self([a, b, c, d])
+    
+
+    /// Creates a new Euui from a provided array of 4 big-endian `u128` GUIDs.
+    ///
+    /// ## Arguments
+    ///
+    /// * `guids` - An array of 4 `u128` values to initialize the Euui.
+    ///
+    /// ## Returns
+    ///
+    /// A new `Euui` instance containing the given GUIDs in big-endian format.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// use euui::Euui;
+    ///
+    /// let guids = [
+    ///     0x1234567890abcdef1234567890abcdef,
+    ///     0xabcdef1234567890abcdef1234567890,
+    ///     0x7890abcdef1234567890abcdef123456,
+    ///     0x567890abcdef1234567890abcdef1234,
+    /// ];
+    /// let euui = Euui::from_be_guids(guids);
+    ///
+    /// assert_eq!(euui.to_be_guids(), guids);
+    /// ```
+    pub fn from_be_guids(guids: [u128; 4]) -> Self {
+        Self(guids)
     }
 
-    /// Gets one of the 4 u128s composing this EUUI.
+
+    /// Creates a new Euui from a provided array of 64 big-endian bytes.
+    ///
+    /// ## Arguments
+    ///
+    /// * `bytes` - An array of 64 bytes to initialize the Euui.
+    ///             Each 16-byte segment in the array is treated as a single `u128`
+    ///             in big-endian format, resulting in a total of 4 `u128` values.
+    ///
+    /// ## Returns
+    ///
+    /// A new `Euui` instance containing the given bytes as 4 big-endian `u128` values.
+    ///
+    /// ## Example
+    ///
+    /// ```rust
+    /// use euui::Euui;
+    ///
+    /// let bytes = [
+    ///     0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF,
+    ///     0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF,
+    ///     0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x90,
+    ///     0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x90,
+    ///     0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56,
+    ///     0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56,
+    ///     0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34,
+    ///     0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34,
+    /// ];
+    /// let euui = Euui::from_be_bytes(bytes);
+    ///
+    /// assert_eq!(euui.to_be_bytes(), bytes);
+    /// ```
+    pub fn from_be_bytes(bytes: [u8; 64]) -> Self {
+        let mut guids = [0u128; 4];
+        for i in 0..4 {
+            guids[i] = u128::from_be_bytes(bytes[i * 16..(i + 1) * 16].try_into().expect("Logic error"));
+        }
+        Self(guids)
+    }
+
+    /// Gets one of the 4 u128s composing this Euui.
     ///
     /// Returns [None] if index >= 4.
     pub fn u128(&self, index: usize) -> Option<u128> {
         if index >= self.0.len() { None } else { Some(self.0[index]) }
     }
 
-    /// Gets one of the 64 u8s composing this EUUI.
+    /// Gets one of the 64 u8s composing this Euui.
     ///
     /// Returns [None] if index >= 64.
     pub fn u8(&self, index: usize) -> Option<u8> {
@@ -78,7 +167,7 @@ impl Euui {
         }
     }
 
-    /// Returns the 64 u8s composing this EUUI.
+    /// Returns the 64 u8s composing this Euui.
     pub fn to_be_bytes(&self) -> [u8; 64] {
         let mut bytes = [0u8; 64];
         for i in 0..64 {
@@ -87,11 +176,11 @@ impl Euui {
         bytes
     }
 
-    /// Returns the 4 u128s composing this EUUI.
+    /// Returns the 4 u128s composing this Euui.
     ///
     pub fn to_be_guids(&self) -> [u128; 4] { self.0 }
 
-    /// Gets one of the 8 u64s composing this EUUI.
+    /// Gets one of the 8 u64s composing this Euui.
     ///
     /// Returns [None] if index >= 8.
     pub fn u64(&self, index: usize) -> Option<u64> {
@@ -102,7 +191,7 @@ impl Euui {
         }
     }
 
-    /// Returns a hexadecimal formatted EUUI which follows this pattern (given #x is `self.0[x - 1]`) :
+    /// Returns a hexadecimal formatted Euui which follows this pattern (given #x is `self.0[x - 1]`) :
     /// ```txt
     /// #1-#2
     /// #3-#4
@@ -123,7 +212,18 @@ impl Euui {
 }
 
 impl Display for Euui {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+
+    /// Returns a hexadecimal Euui in one block. It follows this pattern (given #x is `self.0[x - 1]`) :
+    /// ```txt
+    /// #1#2#3#4
+    /// ```
+    ///
+    /// ## Example
+    ///
+    /// ```txt
+    /// 2f8596cc2f3b3da9adf20cf9413104ab1f8de1116aef039d12c80587e7551080d43ed7632e94801a395a5454a382dff123decf62d51eafee3ec0bb98b1b90d15
+    /// ```
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         write!(
             f,
             "{:032x}{:032x}{:032x}{:032x}",
@@ -134,6 +234,7 @@ impl Display for Euui {
 
 #[cfg(test)]
 mod tests {
+    use alloc::string::ToString;
     use crate::Euui;
 
     #[test]
@@ -152,11 +253,5 @@ mod tests {
             euui.to_string(),
             "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
         );
-    }
-
-    #[test]
-    fn format_random() {
-        let euui = Euui::random();
-        println!("{}\n\n{}", euui.format(), euui);
     }
 }
